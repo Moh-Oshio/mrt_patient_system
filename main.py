965bcc177fdd554608c88dad3276ef7b5438aa23
Mohammed Yahayah <mohammedyahayah@gmail.com>
2025-12-13 23:11:33 +0100

    Added a method in the Reception class for generating random patient card numbers.

--- main.py
+++ main.py
@@ -10,29 +10,30 @@ card_nums = []
 
 
 class Reception:
-    # Initializes the system by reading existing file numbers and creating the patient directory
+    """Initializes the system by reading existing file numbers and creating the patient directory"""
 
     def __init__(self):
-        # This attempts to load previously generated file numbers from the .txt file storing the file numbers.
         try:
             with open('file_numbers.txt', 'r') as file:
                 for line in file:
                     number = line.strip()
                     if number and number not in card_nums:
                         card_nums.append(number)
-        # If the file does not exist, an empty list is initialized.
         except FileNotFoundError:
             pass
 
         self.folder = 'patients'
 
-        # This ensures the 'patients' folder exists. If it does, individual patient records are stored in it.
-
         os.makedirs(self.folder, exist_ok=True)
 
+    def num_gen(self):
+        rand_num = str(randint(100, 5000))
+        def_len = 6
+        new_num = (((def_len - len(rand_num)) * '0') + rand_num)
 
-# In addition to the code in the constructor, methods for:
-# - generating a random card number
-# - creating a new patient card using the last generated number as the new patient card number
-# - checking a patient file using the card number
-# - recording vital signs of the patient upon entry into the hospital
+        card_nums.append(new_num)
+
+        with open('file_numbers.txt', 'a') as file:
+            file.write(f'{new_num}\n')
+
+        return new_num
